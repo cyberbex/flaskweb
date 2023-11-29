ffbbb7d211ed467919b72d60829514630169a951
Bruno Migliorini <cyberbex@gmail.com>
2023-11-29 18:36:02 +0000

    trabalhando com  a api tmb

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import urllib.request
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -17,11 +20,29 @@ def principal():
 @app.route('/sobre', methods=["GET", "POST"])
 def sobre():
 	#notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno": 7.0, "Sicrano":8.5, "Rodrigo":9.5}
+	simbolo = "gamba"
 	if request.method == "POST":
 		if request.form.get("aluno") and request.form.get("nota"):
 			registros.append({"aluno": request.form.get("aluno"),"nota": request.form.get("nota")})
+		if request.form.get("simbol"):
+			simbolo = request.form.get("simbol")
 
-	return render_template("sobre.html", registros=registros)
+	return render_template("sobre.html", registros=registros,simbolo=simbolo)
+
+@app.route('/trade', methods=["GET", "POST"])
+def trade():
+	return render_template("trade.html")
+
+@app.route('/filmes')
+def filmes():
+	url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7d7d7e53256c4139b6fb309fcc1cb74f"
+	resposta = urllib.request.urlopen(url)
+
+	dados = resposta.read()
+
+	jsondata = json.loads(dados)
+
+	return render_template("filmes.html",filmes=jsondata['results'])
 
 if __name__ == "__main__":
 	app.run(debug=True)
